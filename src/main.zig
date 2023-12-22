bbd4629d55b185c5b356c5846e400dbc8efd722b
JustTaken <juub_camara@hotmail.com>
2023-12-22 13:17:09 -0400

    Remove flake.lock from .gitignore

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,7 @@
 const std = @import("std");
 
-const hash = '#';
-
 pub fn main() !void {
     const file = try std.fs.cwd().openFile("assets/input.txt", .{});
-    defer file.close();
 
     var buf_reader = std.io.bufferedReader(file.reader());
     var stream = buf_reader.reader();
@@ -12,32 +9,37 @@ pub fn main() !void {
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    var map = try allocator.alloc([2]usize, 0);
+    var map = std.ArrayList([2]usize).init(allocator);
+
     defer {
-        allocator.free(map);
+        file.close();
+        map.deinit();
         const status = gpa.deinit();
         if (status == .leak) {
-            @panic("Could not de initilize the memory\n");
+            @panic("Could not de-initilize the memory\n");
         }
     }
+
     var line_num: usize = 0;
     var col: usize = 0;
-    var index: usize = 0;
+    var empty_lines = [_]bool {true, true, true, true, true, true, true, true, true, true, true, true, };
+    var empty_cols = [_]bool {true, true, true, true, true, true, true, true, true, true, true, true, true, };
 
     while (try stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         for (line) |c| {
-            if (hash == c) {
-                map = try allocator.realloc(map, index + 1);
-                map[index] = .{line_num, col};
-                index += 1;
+            if ('#' == c) {
+                try map.append(.{line_num, col});
+                empty_lines[line_num] = false;
+                empty_cols[col] = false;
             }
             col += 1;
         }
+
         col = 0;
         line_num += 1;
     }
 
-    std.debug.print("{d}\n", .{map});
+    std.debug.print("{any}", .{empty_lines});
 }
 
 test "simple test" {
